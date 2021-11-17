0f352f57228abfa8c5bd5581f982268f2abb9a5f
ratiphi <ratiphi@gmail.com>
2021-11-17 12:31:35 -0800

    Initial commit - starter files

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+//TODO
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
