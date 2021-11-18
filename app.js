b363032b85fea8502952f5677381a7e44ca44f0a
ratiphi <ratiphi@gmail.com>
2021-11-17 17:57:33 -0800

    Add in routes for several API endpoints

--- app.js
+++ app.js
@@ -7,12 +7,114 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({
-  extended: true
+    extended: true
 }));
 app.use(express.static("public"));
 
-//TODO
+// Connect to local MongoDB
+mongoose.connect("mongodb://localhost:27017/wikiDB");
+const articleSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    }
+});
+const Article = mongoose.model("Article", articleSchema);
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+////////////////Requests Targetting All Articles///////////////////
+app.route("/articles")
+    .get(function (req, res) {
+        Article.find({}, function (err, foundArticles) {
+            if (!err) {
+                res.send(foundArticles);
+            } else {
+                res.send(err);
+            }
+        });
+    })
+    .post(function (req, res) {
+        const newArticle = new Article({
+            title: req.body.title,
+            content: req.body.content
+        });
+        newArticle.save(function (err) {
+            if (!err) {
+                res.send("Successfully added a new article.");
+            } else {
+                res.send(err);
+            }
+        });
+    })
+    .delete(function (req, res) {
+        Article.deleteMany({}, function (err) {
+            if (!err) {
+                res.send("Successfully deleted all articles.");
+            } else {
+                res.send(err);
+            }
+        });
+    });
+
+////////////////Requests Targetting A Specific Article///////////////////
+app.route("/articles/:articleTitle")
+    .get((req, res) => {
+        Article.findOne({
+            title: req.params.articleTitle
+        }, (err, foundArticle) => {
+            if (foundArticle) {
+                res.send(foundArticle);
+            } else {
+                res.send("No articles matching that title was found.");
+            }
+        });
+    })
+    .put((req, res) => {
+        Article.replaceOne({
+            title: req.params.articleTitle
+        }, {
+            title: req.body.title,
+            content: req.body.content
+        }, (err) => {
+            if (!err) {
+                res.send("Successfully updated article.");
+            } else {
+                res.send("Error updating article.");
+                console.log(err);
+            }
+        });
+    })
+    .patch((req, res) => {
+        Article.updateOne({
+            title: req.params.articleTitle
+        }, {
+            title: req.body.title,
+            content: req.body.content
+        }, (err) => {
+            if (!err) {
+                res.send("Successfully updated article.");
+            } else {
+                res.send("Error updating article.");
+                console.log(err);
+            }
+        });
+    })
+    .delete((req, res) => {
+        Article.deleteOne({
+            title: req.params.articleTitle
+        }, (err) => {
+            if (!err) {
+                res.send("Successfully deleted article.")
+            } else {
+                res.send("Error deleting article");
+                console.log(err);
+            }
+        });
+    });
+
+app.listen(3000, function () {
+    console.log("Server started on port 3000");
 });
